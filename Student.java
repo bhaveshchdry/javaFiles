e975c5aa2421249a870a0f282ea43ff64e291a8b
Bhavesh Chaudhary <79908497+bhaveshchdry@users.noreply.github.com>
2021-09-15 21:20:47 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,32 @@
+//this is the example of secured code i.e, how we make data private and then how can we access them through a validation process
+
+public class Student {
+    private int age;
+    private char gender;
+
+    public int getAge() {         //for getting the age
+        //we can implement a validation process here i.e, if the person is valid then only it'll return the value
+        return age;
+    }
+
+    public void setAge(int age) {      //for setting the age
+        this.age = age;
+    }
+
+    public char getGender() {            //for getting the gender
+        //we can implement a validation process here i.e, if the person is valid then only it'll return the value
+        return gender;
+    }
+
+    public void setGender(char gender) {       //for setting the gender
+        this.gender = gender;
+    }
+}
+class StudentMain{
+    public static void main(String[] args) {
+        Student bhavesh= new Student();
+        bhavesh.setAge(19);      //setting age
+        bhavesh.setGender('M');  //setting gender
+        System.out.println(bhavesh.getAge()+ "        "+ bhavesh.getGender());    //now calling getters for getting data and print them
+    }
+}
